01435cce258505b4d55dd9dda5e96c63c382ab85
Fergus Deffely <fergdeff@gmail.com>
2021-09-02 18:18:27 +0100

    Adding backup of sample usermanagement database.

---
+++ Database/usermanagment-backup.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: usermanagement
+-- ------------------------------------------------------
+-- Server version	8.0.23
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `email` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `administrator` int NOT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  `org_group` varchar(100) DEFAULT NULL,
+  `image` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('andrea.rossi@groundworks.com','Andrea Rossi','+353 89 2998773',1,'pass4','I.T.',NULL),('ciara.orourke@groundworks.com','Ciara O\'Rourke','+353 86 2254876',0,'pass5','Development',NULL),('ger.moran@groundworks.com','Ger Moran','+353 87 3267891',0,'pass1','Sales',NULL),('lisa.dennehy@groundworks.com','Lisa Dennehy','+353 86 7224005',0,'pass8','Operations',NULL),('lisa.murphy@groundworks.com','Lisa Murphy','+353 86 2188746',0,'pass2','Marketing',NULL),('mahmed.almoud@groundworks.com','Mahmed Almoud','+353 85 3987889',0,'pass9','I.T.',NULL),('robert.malone@groundworks.com','Robert Malone','+353 87 9901011',0,'pass7','Accounts',NULL),('ronan.cotter@groundworks.com','Ronan Cotter','+353 87 3365990',0,'pass3','Human Resources',NULL),('tom.walsh@groundworks.com','Tom Walsh','+353 89 8366021',1,'pass6','Quality Assurance',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-02 17:37:22

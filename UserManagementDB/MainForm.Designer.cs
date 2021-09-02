7602f3f91c7244ae5111e197b398defd5064d548
Fergus Deffely <fergdeff@gmail.com>
2021-09-02 18:13:00 +0100

    Adding initial version of UserManagementDB project.

---
+++ UserManagementDB/MainForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace UserManagementDB
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonQuery = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(99, 94);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(97, 37);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonQuery
+            // 
+            this.buttonQuery.Location = new System.Drawing.Point(99, 137);
+            this.buttonQuery.Name = "buttonQuery";
+            this.buttonQuery.Size = new System.Drawing.Size(97, 37);
+            this.buttonQuery.TabIndex = 1;
+            this.buttonQuery.Text = "Query";
+            this.buttonQuery.UseVisualStyleBackColor = true;
+            this.buttonQuery.Click += new System.EventHandler(this.buttonQuery_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(99, 266);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(97, 37);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(99, 223);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(97, 37);
+            this.buttonUpdate.TabIndex = 3;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.Location = new System.Drawing.Point(99, 180);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(97, 37);
+            this.buttonInsert.TabIndex = 4;
+            this.buttonInsert.Text = "Insert";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.DarkCyan;
+            this.label1.Location = new System.Drawing.Point(46, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Users Database Test";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 349);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonQuery);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "MainForm";
+            this.Text = "User Management Database Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonQuery;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ UserManagementDB/MainForm.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace UserManagementDB
+{
+    public partial class MainForm : Form
+    {
+        DBConnection db = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            this.db = new DBConnection("localhost", "usermanagement", "csharp", "password");
+
+            if (this.db.OpenConnection())
+            {
+                MessageBox.Show("Connected to database.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Problem connecting to database.", "Connection failed", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonQuery_Click(object sender, EventArgs e)
+        {
+            UsersDB usersDB = new UsersDB();
+
+            List<User> userList = usersDB.GetUsers(this.db);
+
+            string userRecordsMessage = "The users in the database are:\n\n";
+
+            foreach(User user in userList)
+            {
+                userRecordsMessage += $"{user.ToString()}\n\n";
+            }
+
+            MessageBox.Show(userRecordsMessage);
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            User user = new User("aaaa@aaa.com", "aaaa", "12345", false, "aaaa", "aaaa", string.Empty);
+
+            UsersDB usersDB = new UsersDB();
+
+            if(usersDB.Insert(this.db, user))
+            {
+                MessageBox.Show($"Successfully inserted record in users table:\n\n{user.ToString()}", "Insert Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            User user = new User("bbbb@bbb.com", "aaaa", "12345", false, "aaaa", "aaaa", null);
+
+            UsersDB usersDB = new UsersDB();
+
+            if (usersDB.Update(this.db, "aaaa@aaa.com", user))
+            {
+                MessageBox.Show($"Successfully updated record in users table:\n\n{user.ToString()}", "Update Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            UsersDB usersDB = new UsersDB();
+
+            if (usersDB.Delete(this.db, "bbbb@bbb.com"))
+            {
+                MessageBox.Show($"Successfully deleted record in users table:\n\n", "Delete Successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.db.CloseConnection();
+        }
+    }
+}
